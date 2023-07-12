165d4198d7333cf497ca3e1989c37dab1ae7bfd1
hongsh429 <shh0429@kakao.com>
2023-07-12 13:11:02 +0900

    조회 API 컨트롤러 개발 및 profile나누기 (local, test)

---
+++ src/main/java/study/querydsl/controller/InitMember.java
@@ -0,0 +1,50 @@
+package study.querydsl.controller;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+
+/*
+스프링 부트가 동작하면, local프로파일에 해당하는 설정을 읽어드린다.
+2023-07-12T08:48:39.064+09:00  INFO 22932 --- [           main] study.querydsl.QuerydslApplication       : The following 1 profile is active: "local"
+*/
+@Profile(value = "local")
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init() {
+        initMemberService.init();
+    }
+
+    @Component
+    static class InitMemberService {
+        @PersistenceContext
+        EntityManager em;
+
+        @Transactional
+        public void init() {
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+            em.persist(teamA);
+            em.persist(teamB);
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member"+i, i, selectedTeam));
+            }
+        }
+    }
+
+}
